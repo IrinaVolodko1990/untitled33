9c01dd2d1086cd4fb57df56244eab57e88c99d3d
Irina_Volodko <volodko.pp.1.20@gmail.com>
2021-02-02 20:18:01 +0300

    Initial commit

---
+++ src/p1/Demo.java
@@ -0,0 +1,11 @@
+package p1;
+
+import java.util.Arrays;
+class ArraysCopy {
+    public static void main(String[] args) {
+        float [] numbers = {167.5f, -2, 16.6f, 99.8f, 26, 92, 43.4f,
+                -234, 35, 80};
+        float [] numbersCopy = Arrays.copyOf(numbers, numbers.length);
+        System.out.println(Arrays.toString(numbersCopy));
+        System.out.println("01-02-2021");
+    }}
